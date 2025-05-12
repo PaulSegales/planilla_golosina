73bbc6ae230f3ef7c77471cc6e62312e7d977c5f
Paul Quispe Segales <88732374+PaulSegales@users.noreply.github.com>
2025-05-12 09:09:26 -0500

    Update Mantenimiento.jsx

--- src/components/Mantenimiento.jsx
+++ src/components/Mantenimiento.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Mantenimiento = () => {
   return (
     <div>
-        {alert("Actualmente la pagina esta en mantenimiento y el perido de prueba termino, se ofrece las disculapas del caso, los calculos de planilla las devera hacerlas a mano, gracias por su comprension XD...!!!")}
+        {alert("Actualmente la página esta en mantenimiento y el periodo de prueba termino, se ofrece las disculapas del caso, los calculos de planilla las deverá hacerlas a mano, gracias por su comprension XD...!!!")}
     </div>
   )
 }
