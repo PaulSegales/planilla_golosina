57c66d933203c87d9542c74c756fa9f451468b51
PaulSegales <1626629@utp.edu.pe>
2026-01-18 22:12:37 -0500

    Cambio del orden de los multiplicadores de dinero

--- src/components/ContadorDinero.jsx
+++ src/components/ContadorDinero.jsx
@@ -57,17 +57,17 @@ const ContadorDinero = ({i, extraerResultado}) => {
     <div className="contenedor_planilla-dinero px-3">
       <h6 className="pt-3">Dinero Contabilizado:</h6>
       <h5 className="monto">S/. {dineroTotal}</h5>
-      <MultiplicadorDinero index={0}  nombre={'0.10'} valorMoneda={0.1} extraerMontoDinero={extraerMontoDinero}/> 
-      <MultiplicadorDinero index={1}  nombre={'0.20'} valorMoneda={0.2} extraerMontoDinero={extraerMontoDinero}/>
-      <MultiplicadorDinero index={2}  nombre={'0.50'} valorMoneda={0.5} extraerMontoDinero={extraerMontoDinero}/>
-      <MultiplicadorDinero index={3}  nombre={'1.00'} valorMoneda={1} extraerMontoDinero={extraerMontoDinero}/> 
-      <MultiplicadorDinero index={4}  nombre={'2.00'} valorMoneda={2} extraerMontoDinero={extraerMontoDinero}/>
-      <MultiplicadorDinero index={5}  nombre={'5.00'} valorMoneda={5} extraerMontoDinero={extraerMontoDinero}/>
-      <MultiplicadorDinero index={6}  nombre={'10.00'} valorMoneda={10} extraerMontoDinero={extraerMontoDinero}/> 
-      <MultiplicadorDinero index={7}  nombre={'20.00'} valorMoneda={20} extraerMontoDinero={extraerMontoDinero}/>
-      <MultiplicadorDinero index={8}  nombre={'50.00'} valorMoneda={50} extraerMontoDinero={extraerMontoDinero}/>
-      <MultiplicadorDinero index={9}  nombre={'100.00'} valorMoneda={100} extraerMontoDinero={extraerMontoDinero}/>
       <MultiplicadorDinero index={10}  nombre={'200.00'} valorMoneda={200} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={9}  nombre={'100.00'} valorMoneda={100} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={8}  nombre={'50.00'} valorMoneda={50} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={7}  nombre={'20.00'} valorMoneda={20} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={6}  nombre={'10.00'} valorMoneda={10} extraerMontoDinero={extraerMontoDinero}/> 
+      <MultiplicadorDinero index={5}  nombre={'5.00'} valorMoneda={5} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={4}  nombre={'2.00'} valorMoneda={2} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={3}  nombre={'1.00'} valorMoneda={1} extraerMontoDinero={extraerMontoDinero}/> 
+      <MultiplicadorDinero index={2}  nombre={'0.50'} valorMoneda={0.5} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={1}  nombre={'0.20'} valorMoneda={0.2} extraerMontoDinero={extraerMontoDinero}/>
+      <MultiplicadorDinero index={0}  nombre={'0.10'} valorMoneda={0.1} extraerMontoDinero={extraerMontoDinero}/> 
       <div className="moneda-billete">
         <label className="label-total">Monedas: </label><span className="valor-neto">S/.{monedas.toFixed(2)}</span><label className="label-total">Billetes: </label><span className="valor-neto">S/.{billetes.toFixed(2)}</span>
       </div>
